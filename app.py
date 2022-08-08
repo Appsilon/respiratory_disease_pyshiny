7a6924296e619581b14601e8a4cd823c0793c154
Pavel Demin <paveldemin@me.com>
2022-08-08 23:02:02 +0800

    feat: complete (almost) navbar

--- app.py
+++ app.py
@@ -3,11 +3,7 @@ from shiny import App, ui
 
 app_ui = ui.page_fluid(
     ui.tags.head(
-        ui.tags.link(
-            rel="stylesheet",
-            type="text/css",
-            href="style.css"
-        )
+        ui.tags.link(rel="stylesheet", type="text/css", href="style.css")
     ),
     # top navbar
     ui.tags.div(
@@ -17,24 +13,63 @@ app_ui = ui.page_fluid(
                 ui.tags.div(
                     ui.tags.a(
                         ui.tags.img(
-                            src="static/img/appsilon-logo.png",
-                            height="50px"
+                            src="static/img/appsilon-logo.png", height="50px"
                         ),
-                        href="https://demo.appsilon.com/"
+                        href="https://demo.appsilon.com/",
                     ),
-                    id="logo-top"
+                    id="logo-top",
                 )
             ),
             ui.column(2),
             ui.column(
                 2,
                 ui.tags.div(
-                    id="div-navbar-tabs"
-                )
-            )
+                    ui.tags.div(
+                        ui.input_action_button(
+                            id="tab_map",
+                            label="Map",
+                            class_="navbar-button",
+                        ),
+                        id="div-navbar-map",
+                    ),
+                    ui.tags.div(
+                        ui.input_action_button(
+                            id="tab_plot",
+                            label="Graphs",
+                            class_="navbar-button",
+                        ),
+                        id="div-navbar-plot",
+                    ),
+                    id="div-navbar-tabs",
+                ),
+            ),
+            ui.column(3),
+            ui.column(
+                2,
+                ui.tags.div(
+                    ui.input_switch(
+                        id="dataset", label="Dataset Select", value=True
+                    ),
+                    id="div-navbar-selector",
+                ),
+            ),
+            ui.column(
+                1,
+                ui.tags.div(
+                    ui.input_action_button(
+                        id="info_icon",
+                        label=None,
+                        icon="circle",  # TODO: how to make an icon?
+                        class_="navbar-info",
+                    )
+                ),
+            ),
         ),
         id="div-navbar",
+        class_="navbar-top",
     ),
+    # main area
+    ui.output_ui("display_area"),
     title="Respiratory Disease App",
 )
 
