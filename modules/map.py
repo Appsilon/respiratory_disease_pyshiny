88f670d253143a3848b9a78336422f750698e786
Pavel Demin <paveldemin@me.com>
2022-08-09 18:50:35 +0800

    feat: add country polygons to the map

--- modules/map.py
+++ modules/map.py
@@ -1,3 +1,5 @@
+import random
+import json
 from shiny import ui, module
 from shinywidgets import output_widget, register_widget
 
@@ -10,6 +12,17 @@ from utils.sidebar_text import (
     dataset_information,
     slider_text_map,
 )
+
+
+with open("data/countries.geojson", "r") as f:
+    data = json.load(f)
+
+
+def random_color(feature):
+    return {
+        "color": "black",
+        "fillColor": random.choice(["red", "yellow", "green", "orange"]),
+    }
 
 
 @module.ui
@@ -43,14 +56,25 @@ def map_server(input, output, session):
     map = L.Map(
         # TODO: this is how it's done in tutorial :)
         basemap=L.basemaps.CartoDB.Positron,  # pyright: ignore
-        center=(51.476852, -0.000500),
-        zoom=12,
+        center=(50, 10),
+        zoom=5,
         scroll_wheel_zoom=True,
         min_zoom=3,
         max_zoom=18,
         no_wrap=True,
         layout=Layout(width="100%", height="100%"),
     )
+    geo_json = L.GeoJSON(
+        data=data,
+        style={
+            "opacity": 1,
+            "dashArray": "9",
+            "fillOpacity": 0.1,
+            "weight": 1,
+        },
+        hover_style={"color": "white", "dashArray": "0", "fillOpacity": 0.5},
+        style_callback=random_color,
+    )
     # Add a distance scale
-    map.add_control(L.leaflet.ScaleControl(position="bottomleft"))
+    map.add_layer(geo_json)
     register_widget("map", map)
