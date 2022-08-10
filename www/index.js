93bad0bb21d74a75332d85bb9b941ea87a7ed542
Pedro Silva <pedro@appsilon.com>
2022-08-10 15:05:56 +0200

    feat: Simplify menu toggle.

--- www/index.js
+++ www/index.js
@@ -1,20 +1,5 @@
 $(() => {
-
-  $("#tab_plot").click(() => {
-    window.dispatchEvent(new Event("resize"));
-  });
-
-  $("#tab_map").click(() => {
-    window.dispatchEvent(new Event("resize"));
-  });
-
   Shiny.addCustomMessageHandler("toggleActiveTab", (payload) => {
-    if (payload.activeTab === "map") {
-      $("#map-container").show()
-      $("#plot-container").hide()
-    } else {
-      $("#plot-container").show()
-      $("#map-container").hide()
-    }
+    $(".page-main").toggleClass("main-visible");
   });
 });
