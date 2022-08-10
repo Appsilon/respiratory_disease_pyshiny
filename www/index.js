b30f32702fcba64ebe120e90274d6d42aabab61e
Pavel Demin <paveldemin@me.com>
2022-08-10 16:49:52 +0800

    hotfix: resize charts & map

--- www/index.js
+++ www/index.js
@@ -1,4 +1,13 @@
 $(() => {
+
+  $("#tab_plot").click(() => {
+    window.dispatchEvent(new Event("resize"));
+  });
+
+  $("#tab_map").click(() => {
+    window.dispatchEvent(new Event("resize"));
+  });
+
   Shiny.addCustomMessageHandler("toggleActiveTab", (payload) => {
     if (payload.activeTab === "map") {
       $("#map-container").show()
